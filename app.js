b7d40b210f71af0e84b54a0191c9ee3a412ea0ca
Austinkuria <kuriaaustin125@gmail>
2023-03-11 11:42:14 +0300

    Guess-the-number

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const gameButtons = document.querySelectorAll('.game-btn');
 const gameText = document.getElementsByClassName('game-text');
-const gameInput =document.getElementById('game-input');
+const gameInput = document.getElementById('game-input');
 const gameAlert = document.querySelector('.game-alert');
 const successSound = document.getElementById('correct-answer-sound');
 const highSoundError = document.getElementById('wrong-answer-high-sound');
-const lowSoundError= document.getElementById('wrong-answer-low-sound');
+const lowSoundError = document.getElementById('wrong-answer-low-sound');
 
 let secretNumber;
 const bodyStyling = document.getElementById('body-styling');
@@ -12,54 +12,54 @@ const bodyStyling = document.getElementById('body-styling');
 // == equality operator "20" is equal 20
 // === equality operator "20" is equal not 20
 
-function newGame(){
-secretNumber = Math.floor(Math.random()*300);
-// alert(secretNumber)
-gameAlert.innerText = "A new random number has been generated.Good luck  in guessing it";
+function newGame() {
+    secretNumber = Math.floor(Math.random() * 300);
+    // alert(secretNumber)
+    gameAlert.innerText = "A new random number has been generated.Good luck  in guessing it";
 };
 
-    function checkUserGuess(){
+function checkUserGuess() {
     let userGuess = gameInput.value
-    if(userGuess === ""){
-    gameText[1].innerText = "Provide a number!";
+    if (userGuess === "") {
+        gameText[1].innerText = "Provide a number!";
     }
-    else if (secretNumber === parseInt(userGuess)){
+    else if (secretNumber === parseInt(userGuess)) {
         gameText[1].innerText = "Hurray!!!You found the correct number,you have won"
         successSound.play();
         bodyStyling.style.backgroundColor = "gold";
     }
-    
-    else if(parseInt(userGuess) > secretNumber){
+
+    else if (parseInt(userGuess) > secretNumber) {
         gameText[1].innerText = `${userGuess} is too high!`
         highSoundError.play();
         bodyStyling.style.backgroundColor = "greenyellow";
     }
-    
-     else if(parseInt(userGuess) < secretNumber){
+
+    else if (parseInt(userGuess) < secretNumber) {
         gameText[1].innerText = `${userGuess} is too low!`
         lowSoundError.play();
         bodyStyling.style.backgroundColor = "lightblue";
-        
+
     }
     gameInput.value = "";
     gameAlert.innerText = "";
 }
 
 // we pass by referrence  the function here
-gameButtons[1].addEventListener('click',checkUserGuess)
+gameButtons[1].addEventListener('click', checkUserGuess)
 
-document.addEventListener('keydown',function(event){
+document.addEventListener('keydown', function (event) {
     console.log(event);
-    if(event.key == "Enter"){
-        checkUserGuess ();
+    if (event.key == "Enter") {
+        checkUserGuess();
     }
 });
 
-gameButtons[0].addEventListener('click',newGame)
+gameButtons[0].addEventListener('click', newGame)
 
-document.addEventListener('keydown',function(event){
+document.addEventListener('keydown', function (event) {
     console.log(event)
-if(event.key == 'n'){
-    newGame();
-}
+    if (event.key == 'n') {
+        newGame();
+    }
 });
